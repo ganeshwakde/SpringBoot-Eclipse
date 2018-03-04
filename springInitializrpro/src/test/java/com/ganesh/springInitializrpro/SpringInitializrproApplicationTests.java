3916b271eac5b64e318288b493d66fbad03e7aee
Ganesh Wakde <gwganesh88@gmail.com>
2018-03-04 11:04:45 +0530

    Second coomit for springInitialiserProject

---
+++ springInitializrpro/src/test/java/com/ganesh/springInitializrpro/SpringInitializrproApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ganesh.springInitializrpro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringInitializrproApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
